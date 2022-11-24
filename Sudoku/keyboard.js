6d5fd8138079d9a46445bda1da9015966229f6d8
ishiyan1612 <118957037+ishiyan1612@users.noreply.github.com>
2022-11-24 14:43:30 +0900

    Add files via upload

---
+++ Sudoku/keyboard.js
@@ -0,0 +1,23 @@
+function handleKeydown(e) {
+
+    var key = e.key;
+    //var result = false;
+    //alert(key);               //e.key確認用
+
+    //mode = 1;  
+    /*  
+    if (mode == 1) {
+        if (key === 'ArrowRight') { // 右
+            result = Shift(0, 1);
+        } else if (key === 'ArrowLeft') { // 左
+            result = Shift(0, -1);
+        } else if (key === 'ArrowDown') { //下
+            result = Shift(1, 0);
+        } else if (key === ' ') { //スペース（回転）
+            result = rotationBlock();
+        }
+    }
+
+    if (result) draw();
+    */
+}
